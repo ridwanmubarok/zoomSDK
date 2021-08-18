3dda7b0fe69ec2bed86860861880d6f9ba0c79f9
ridwanmubarok <ridwanmubarok5@gmail.com>
2021-08-18 15:34:11 +0700

    add new classes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>4VM ZOOM</title>
 </head>
-<body>
-    <div id="root custom-zoom">
+<body class="custom-zoom">
+    <div id="root">
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
